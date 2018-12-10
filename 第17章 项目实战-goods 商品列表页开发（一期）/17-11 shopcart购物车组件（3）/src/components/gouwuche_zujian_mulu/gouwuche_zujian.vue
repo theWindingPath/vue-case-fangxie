b72935399f0a8efaa43c67b8a57c8ff7dfe1093e
theWindingPath <2958202145@qq.com>
2018-12-10 12:23:51 +0800

    17-12 shopcart购物车组件（4）

--- 第17章 项目实战-goods 商品列表页开发（一期）/17-11 shopcart购物车组件（3）/src/components/gouwuche_zujian_mulu/gouwuche_zujian.vue
+++ 第17章 项目实战-goods 商品列表页开发（一期）/17-11 shopcart购物车组件（3）/src/components/gouwuche_zujian_mulu/gouwuche_zujian.vue
@@ -7,14 +7,10 @@
                       <i class="icon-shopping_cart"></i>
                   </div>
               </div>
-              <div class="price">￥{{jiage_zongshu_computed}} / {{zonggeshu_computed}}个</div>
+              <div class="price">￥0</div>
               <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
           </div>
-          <div class="content-right">
-              <div class="pay">
-                  ￥{{minPrice}}起送
-              </div>
-          </div>
+          <div class="content-right">￥{{minPrice}}起送</div>
       </div>
   </div>
 </template>
@@ -22,21 +18,6 @@
 <script type="text/ecmascript-6">
   export default {
       props: {
-          xuanzhe_shangpin_props: { // 维护选择商品数组，来获得商品数量
-              type: Array, // 数组
-              default() {
-                  return [
-                      {
-                          danjia_shuxing: 12, // 单价
-                          shuliang_shuxing: 1 // 数量
-                      },
-                      {
-                          danjia_shuxing: 10,
-                          shuliang_shuxing: 1
-                      }
-                  ];
-              }
-          },
           deliveryPrice: {
               type: Number,
               default: 0
@@ -44,22 +25,6 @@
           minPrice: {
               type: Number,
               default: 0
-          }
-      },
-      computed: {
-          jiage_zongshu_computed() { // 计算价格总数的函数
-              let zongjiage_bianliang = 0;
-              this.xuanzhe_shangpin_props.forEach((shangpin_xingcan) => { // 遍历数组xuanzhe_shangpin，获得所有元素里的price和count
-                zongjiage_bianliang += shangpin_xingcan.danjia_shuxing * shangpin_xingcan.shuliang_shuxing; // 累加单价*数量，获得总价
-              });
-              return zongjiage_bianliang;
-          },
-          zonggeshu_computed() { // 选择的总个数
-            let zonggeshu_bianliang = 0;
-            this.xuanzhe_shangpin_props.forEach((shangpin_xingcan) => { // 获得每个food对象
-              zonggeshu_bianliang += shangpin_xingcan.shuliang_shuxing; // 累加每个选中的数量count
-            });
-            return zonggeshu_bianliang;
           }
       }
   };
@@ -118,16 +83,9 @@
                   vertical-align top
                   margin 12px 12px 0 12px
                   line-height 24px
-                  font-size 10px
+                  font-size 12px
           .content-right
               flex 0 0 105px
               width 105px
-              .pay
-                height 48px
-                line-height 48px
-                text-align center
-                font-size 12px
-                font-weight 700
-                background-color #2b333b
-                
+              font-size 16px
 </style>
